5f4ade2da82f657d2d11bdd3c07f81054ea67bf0
Renato Mateus <renatomateusx@gmail.com>
2021-02-01 09:57:27 -0300

    Uploading From GDrive to Git Repo

--- Random Numbers/ViewController.swift
+++ Random Numbers/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var generatedNumbers: UILabel!
+    @IBOutlet weak var btnGenerateNumbers: UIButton!
+    
+    @IBAction func generateNumber(_ sender: Any) {
+        let number = arc4random_uniform(11)
+        generatedNumbers.text = String(number)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
